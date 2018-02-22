3e1bb2d4a6bef118b131b4e0031543133ae428a4
Norbert <mail@wilmie.de>
2018-02-22 19:45:48 +0100

    Bugfix: getfhem.php und setfhem.php wieder auf Demowerte gesetzt.

--- demo/setfhem.php
+++ demo/setfhem.php
@@ -1,28 +1,3 @@
 <?php
-//Hostname und Telnet-Port des FHEM-Servers
-$fhemhost = "localhost";
-$fhemport = 7072;
-session_start();
-require_once("/sd_p2/web/php_inc/config.inc.php");
-require_once("/sd_p2/web/php_inc/functions.inc.php");
-
-if(is_checked_in()) {
-	if (isset($_GET["geraet"])) {  $geraet=$_GET["geraet"]; }
-	if (isset($_GET["eigenschaft"])) {  $eigenschaft=$_GET["eigenschaft"]; }
-	if (isset($_GET["wert"])) {  $wert=$_GET["wert"]; } else { $wert=""; }
-	if (isset($_GET["wert1"])) {  $wert1=$_GET["wert1"]; } else { $wert1=""; }
-//Socket öffnen
-	$fhemsock = fsockopen($fhemhost, $fhemport, $errno, $errstr, 30);
-	$fhemcmd = "set ".$geraet." ".$eigenschaft." ".$wert." ".$wert1." \r\nquit\r\n";
-//print $fhemcmd."<br>";
-	fwrite($fhemsock, $fhemcmd);
-	while(!feof($fhemsock)) {
-		$ergebnis=fgets($fhemsock, 128);
-	}
-#print $geraet." gesetzt auf ".$eigenschaft." = ".$wert." ".$wert1." ";
-	echo $fhemcmd;
-} else {
-	echo "no login";
-}	
-
+print " Dies ist nur eine Demo !!!!! ";
 ?>
