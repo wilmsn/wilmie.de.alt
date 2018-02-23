a4e1f86930f1d2859cf611cf7c6a8bfe6314ff9c
Norbert <mail@wilmie.de>
2018-02-23 16:49:36 +0100

    rf24hubadm: savenode und savesensor abgesichert gegen fehlende Eingaben

--- admin/savenode.php
+++ admin/savenode.php
@@ -5,32 +5,28 @@ if (isset($_GET["onid"])) { $onid=$_GET["onid"]; } else { $onid=999999999; }
 if (isset($_GET["nid"])) { $nid=$_GET["nid"]; } else { $nid=999999999; }
 if (isset($_GET["nn"])) { $nn=$_GET["nn"]; } else { $nn=" "; }
 if (isset($_GET["ni"])) { $ni=$_GET["ni"]; } else { $ni=" "; }
-if (isset($_GET["st1"])) { $st1=$_GET["st1"]; } else { $st1=60; }
-if (isset($_GET["st2"])) { $st2=$_GET["st2"]; } else { $st2=60; }
-if (isset($_GET["st3"])) { $st3=$_GET["st3"]; } else { $st3=2; }
-if (isset($_GET["st4"])) { $st4=$_GET["st4"]; } else { $st4=2; }
-if (isset($_GET["rm"])) { $rm=$_GET["rm"]; } else { $rm=0; }
-if (isset($_GET["vd"])) { $vd=$_GET["vd"]; } else { $vd=1; }
-if (isset($_GET["bid"])) { $bid=$_GET["bid"]; } else { $bid=0; }
-
+if (isset($_GET["st1"])) { $st1=$_GET["st1"]; } 
+if ( $st1*1 == 0 ) { $st1=60; }
+if (isset($_GET["st2"])) { $st2=$_GET["st2"]; } 
+if ( $st2*1 == 0 ) { $st2=60; }
+if (isset($_GET["st3"])) { $st3=$_GET["st3"]; } 
+if ( $st3*1 == 0 ) { $st3=60; }
+if (isset($_GET["st4"])) { $st4=$_GET["st4"]; } 
+if ( $st4*1 == 0 ) { $st4=60; }
+if (isset($_GET["rm"])) { $rm=$_GET["rm"]; } 
+$rm=$rm*1; 
+if (isset($_GET["vd"])) { $vd=$_GET["vd"]; }
+if ( $vd*1 == 0 ) { $vd=1; }
+if (isset($_GET["bid"])) { $bid=$_GET["bid"]; } 
+$bid=$bid*1;
 
 if ( $onid == 0 ) {
-	$sensorhub_db->query("insert into node (node_id, node_name, add_info, sleeptime1, sleeptime2, sleeptime3, sleeptime4, radiomode, voltagecorrection, battery_id) ".
-						 " values('".$nid."', '".$nn."', '".$ni."', ".$st1.", ".$st2.", ".$st3.", ".$st4.", ".$rm.", ".$vd.", ".$bid.") "); 
-	print "Neuen Node angelegt, NodeID: ".$nid." insert into node (node_id, node_name, add_info, sleeptime1, sleeptime2, sleeptime3, sleeptime4, radiomode, voltagecorrection, battery_id) ".
-						 " values('".$nid."', '".$nn."', '".$ni."', ".$st1.", ".$st2.", ".$st3.", ".$st4.", ".$rm.", ".$vd.", ".$bid.") "; 
+$sql ="insert into node (node_id, node_name, add_info, sleeptime1, sleeptime2, sleeptime3, sleeptime4, radiomode, voltagecorrection, battery_id) values('".$nid."', '".$nn."', '".$ni."', ".$st1.", ".$st2.", ".$st3.", ".$st4.", ".$rm.", ".$vd.", ".$bid.")";
+	$sensorhub_db->query($sql);
+	print "Neuen Node angelegt, NodeID: ".$nid;
 } else {
-	$sensorhub_db->query(" update node set ".
-						"node_name = '".$nn."', ".
-						"add_info = '".$ni."', ".
-						"sleeptime1 = ".$st1.", ".
-						"sleeptime2 = ".$st2.", ".
-						"sleeptime3 = ".$st3.", ".
-						"sleeptime4 = ".$st4.", ".	
-						"radiomode = ".$rm.", ".
-						"voltagecorrection = ".$vd.", ".
-						"battery_id = ".$bid." ".
-						" where node_id = '".$onid."' "); 
+	$sql = " update node set node_name = '".$nn."', add_info = '".$ni."', sleeptime1 = ".$st1.", sleeptime2 = ".$st2.",sleeptime3 = ".$st3.",sleeptime4 = ".$st4.", radiomode = ".$rm.", voltagefactor = ".$vd.", battery_id = ".$bid."  where node_id = '".$onid."' ";
+        $sensorhub_db->query($sql);
 	print "Update erfolgt für NodeID: ".$onid;
 }					   
 ?>
--- admin/savesensor.php
+++ admin/savesensor.php
@@ -7,20 +7,17 @@ if (isset($_GET["sn"])) { $sn=$_GET["sn"]; } else { $sn=" "; }
 if (isset($_GET["si"])) { $si=$_GET["si"]; } else { $si=" "; }
 if (isset($_GET["nid"])) { $nid=$_GET["nid"]; } else { $nid="00"; }
 if (isset($_GET["ch"])) { $ch=$_GET["ch"]; } else { $ch=0; }
+$ch=$ch*1;
 if (isset($_GET["ty"])) { $ty=$_GET["ty"]; } else { $ty=0; }
+$ty=$ty*1;
 
 if ( $osid == 0 ) {
-	$sensorhub_db->query(" insert into sensor (sensor_id, sensor_name, add_info, node_id, channel, type)  ".
-						 " values(".$sid.",'".$sn."', '".$si."', '".$nid."', ".$ch.", '".$ty."' )"); 
+	$sql = "insert into sensor (sensor_id, sensor_name, add_info, node_id, channel, type) values(".$sid.",'".$sn."', '".$si."', '".$nid."', ".$ch.", '".$ty."' )";
+	$sensorhub_db->query($sql);
 	print "Neuen Sensor angelegt: ".$sid;
 } else {
-	$sensorhub_db->query(" update sensor set ".
-						 "sensor_name = '".$sn."', ".
-						 "add_info = '".$si."', ".
-						 "node_id = '".$nid."', ".
-						 "channel = ".$ch.", ".
-						 "type = '".$ty."' ".
-						 " where sensor_id = ".$osid." "); 
+	$sql = "update sensor set sensor_name = '".$sn."', add_info = '".$si."', node_id = '".$nid."', channel = ".$ch.", type = '".$ty."' where sensor_id = ".$osid;
+        $sensorhub_db->query($sql);
 	print "Update erfolgt für SensorID: ".$osid; 
 }
 ?>
